e982c8a83c7edf10a4372d28b276af7239071d57
Ravi879 <gadhiyara@gmail.com>
2020-09-06 18:21:31 +0530

    Add request context listener

--- src/main/java/com/thinkitive/cruddemo/CrudDemoApplication.java
+++ src/main/java/com/thinkitive/cruddemo/CrudDemoApplication.java
@@ -2,12 +2,20 @@ package com.thinkitive.cruddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.request.RequestContextListener;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class CrudDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CrudDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CrudDemoApplication.class, args);
+    }
 
+    @Bean
+    public RequestContextListener requestContextListener() {
+        return new RequestContextListener();
+    }
 }
