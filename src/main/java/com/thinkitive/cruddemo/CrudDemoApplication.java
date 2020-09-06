579cf5d3690a1d3c8217302e8b9b84ba80aa533b
Ravi879 <gadhiyara@gmail.com>
2020-09-06 18:23:27 +0530

    Add comment

--- src/main/java/com/thinkitive/cruddemo/CrudDemoApplication.java
+++ src/main/java/com/thinkitive/cruddemo/CrudDemoApplication.java
@@ -14,6 +14,8 @@ public class CrudDemoApplication {
         SpringApplication.run(CrudDemoApplication.class, args);
     }
 
+    // credits:
+    // https://stackoverflow.com/questions/35875098/protecting-rest-api-with-oauth2-error-creating-bean-with-name-scopedtarget-oau
     @Bean
     public RequestContextListener requestContextListener() {
         return new RequestContextListener();
